b3f8b5222c1671a4cfc419a37db437feccfce2de
Dungkho <32971948+Dungkho@users.noreply.github.com>
2020-06-22 10:16:57 +0700

    Update my.js

--- my.js
+++ my.js
@@ -8,4 +8,25 @@ function  testGS(){
         });
 }
 
+function  AddGS(){
+    var url = "https://script.google.com/macros/s/AKfycbxcgfPJ4x2acshGYltLcK6nGxSUwM_ples-OksjPSiSWutPuPTV/exec"
+
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'omit', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"John"}) // body data type must match "Content-Type" header
+  });
+        
+        
+}
+
+document.getElementById("btn2").addEventListener("click", AddGS);
 document.getElementById("btn").addEventListener("click", testGS);
